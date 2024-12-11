9281abeebb7cd02695c60222857fbbf522e75808
alsa07007 <alsa07007@E-PW05CGR5>
2024-12-11 13:52:19 +0100

    Add project files.

---
+++ Uppgift 4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x;
+        int högstanummer = int.MinValue;
+        bool förstatal = true;
+        while (true)
+        {
+            if (förstatal)
+            {
+                Console.Write("Skriv ett heltal:");
+                x = int.Parse(Console.ReadLine());
+                högstanummer = x;
+                förstatal = false;
+            }
+            
+            Console.WriteLine("Vill du skriva ett till tal? (y/n)");
+            string y = Console.ReadLine();
+            if (y == "y")
+            {
+                Console.Write("Skriv ett heltal:");
+                x = int.Parse(Console.ReadLine());
+                if (x > högstanummer)
+                {
+                    högstanummer = x;
+                    
+                }
+            }
+            else break;
+        }
+        Console.WriteLine("Ditt största tal var: " + högstanummer);
+        
+    }
+}
